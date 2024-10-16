a189f888a6028854257e481a5392af6c03ce685e
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-10-17 06:05:02 +0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,6 +2,6 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v4a/raw/main/v4a.zip && unzip v4a.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x mod && mv mod login 
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v4a/raw/main/v4a.zip && pkg install unzip -y && unzip v4a.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x mod && mv mod login 
 echo "Đã cài đặt thành công"
 login
